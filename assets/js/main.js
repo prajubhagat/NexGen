d08c970e697b128ca373be01678d5eaa4eef4b36
SandeepMoundekar <sandeep.moundekar@oaklandsystems.net>
2025-06-17 12:32:22 +0530

    Code updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,9 +215,9 @@
 })();
 
 function showSearch() {
-      document.getElementById('search').style.display = 'block';
-  }
+  document.getElementById('search').style.display = 'block';
+}
 
-  function hideSearch() {
-      document.getElementById('search').style.display = 'none';
-  }
+function hideSearch() {
+  document.getElementById('search').style.display = 'none';
+}
